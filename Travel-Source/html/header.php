1aa4afecec9d0a44c47c9c1c658c7ea65d47e8b4
caominhbinh282 <80211677+caominhbinh282@users.noreply.github.com>
2021-09-30 16:24:30 +0700

    Create header.php

---
+++ Travel-Source/html/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.1/css/all.min.css" />
+        <link rel="stylesheet" href="../css/index.css">
+        <script type="text/javascript" src="../javascript/index.js"></script>
+    </head>
+
+    <body>
+
+<nav>
+    <span><a href="#">VIVU .VN</a></span>
+    <ul>
+        <li>
+            <i class="fas fa-pencil-alt"></i>
+            <a>Xem đánh giá</a>
+        </li>
+        <li id="notification">
+            <a>
+                <i class="far fa-bell"></i>
+                Thông báo
+            </a>
+        </li>
+        <li class="login"><a href="">Đăng nhập</a></li>
+    </ul>
+
+<!----------------------->
+
+    <div class="content">
+        <div class="menu">
+            <ul >
+                
+                <li class="menu-item active" >
+                    <a href="html/activity_page.php">  
+                        <span><i class="fas fa-plane-departure"></i></span>
+                        <span class="text"> Du lịch</span>
+                    </a>
+                </li>
+                <li class="menu-item" >
+                    <a>
+                        <span><i class="fas fa-hotel"></i></span>
+                        <span class="text">Khách sạn</span>
+                    </a>
+                </li>
+                
+                <li class="menu-item" >
+                    <a href="html/food-page.php"> 
+                        <span><i class="fas fa-glass-martini"> </i></span>
+                        <span class="text">Nhà hàng</span>
+                    </a>
+                </li>
+                <li class="menu-item" >
+                    <a href="">
+                        <span><i class="fas fa-gopuram"></i></span>
+                        <span class="text">Hoạt động giải trí</span>
+                    </a>
+                </li>
+                <li class="menu-item" >
+                    <a href="">    
+                        <span><i class="fas fa-comment-alt"></i></span>
+                        <span class="text">Bài viết</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    </nav>
+    </body>
+</html>
