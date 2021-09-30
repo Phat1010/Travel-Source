f4a8d19fb663ca7e08c336bcc4b2615bf9944c8c
caominhbinh282 <80211677+caominhbinh282@users.noreply.github.com>
2021-09-30 16:24:25 +0700

    Create activity_page.php

---
+++ Travel-Source/html/activity_page.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Insert title here</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.1/css/all.min.css" />
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/activity.css">
+  
+</head>
+<body>
+
+
+	<div class="head"> <?php require_once 'header.php';?> </div>
+    <div class="container">
+    <div class="header">
+            <div class="row1">
+                <h2>Ghềnh Đá Đĩa</h2>
+                <div class="confirm">
+                    <span><i class="fas fa-check-circle"></i></span>
+                    Đã xác nhận
+                </div>
+                <div class="option">
+                    <div class="save">
+                        <i class="far fa-heart"></i>
+                        <span>Lưu</span>
+                    </div>
+                    <div class="share">
+                        <i class="far fa-share-square"></i>
+                        <span>Chia sẻ</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row2">
+                <div class="rate">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="far fa-star"></i>
+                    <b>   100 đánh giá</b>
+                </div>
+                <div class="type">
+                    <span>Địa danh du lịch nổi tiếng, vẻ đẹp của thiên nhiên</span>
+                </div>
+            </div>
+            
+            <div class="row3">
+                <div >
+                    <span> <i class="fas fa-map-marker-alt"></i></span>
+                    Địa chỉ
+                </div>
+                <div>
+                    <span><i class="fas fa-phone"></i> </span>
+                    0123456789
+                </div>
+                <div class="time">
+                    <span><i class="fas fa-clock"></i> </span>
+                    7h30-18h
+                </div>
+            </div>
+
+
+        </div>
+
+        <!------------------------------------>
+        
+        <div class="main">
+            <div class="rating">
+                <h3>Đánh giá và xếp hạng</h3>
+                <div class="danhgia">
+                    <div class="icon">
+                        <b class="rate-total">4.0</b>
+                    
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="far fa-star"></i>
+                        <b>   100 đánh giá</b>  
+                    </div>
+                    <span>Quy Nhơn-Phú Yên</span>
+                    <span>Xã An Ninh Đông, huyện Tuy An, tỉnh Phú Yên</span>
+                </div>
+                <div class="border"></div>
+                <div class="xephang">
+                    <h4>Xếp hạng</h4>
+                    <div class="row">
+                        <span><i class="fas fa-hamburger"></i></span>
+                        &nbsp Đồ ăn
+                        <span class="xyz">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </span>
+                    </div>
+                    <div class="row">
+                        <span><i class="fas fa-concierge-bell"></i></span>
+                        &nbsp  Dịch vụ
+                        <span class="xyz">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </span>
+                    </div>
+                    <div class="row">
+                        <span><i class="far fa-gem"></i></span>
+                        &nbsp Giá trị
+                        <span class="xyz">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="info">
+                <div class="img-prim">
+                    <img src="../img/images.jpg" class="imgslider">
+                    <button class="btn-prev">
+                        <i class="fas fa-angle-left"></i>
+                    </button>
+                    <button class="btn-next">
+                        <i class="fas fa-angle-right"></i>
+                    </button>
+                </div>
+                <div class="img-items">
+                    <img src="../img/images.jpg" class="imgslider">
+                    <img src="../img/images.jpg" class="imgslider">
+                    <img src="../img/images.jpg" class="imgslider">
+                    <img src="../img/images.jpg" class="imgslider">
+                </div>
+                
+                <div class="xyzz">
+                    <div class="detail">
+                        <div>
+                            <div>
+                                <div>
+                                    <h2>Chi tiết</h2>
+                                </div>
+                                <div>
+                                    <div>
+                                        <div>Địa chỉ:</div>
+                                        <div>Xã An Ninh Đông, huyện Tuy An, tỉnh Phú Yên</div>
+                                    </div>
+                                    <div>
+                                        <div>Vị trí:</div>
+                                        <div>Cách trung tâm thành phố Tuy Hòa, Phú Yên khoảng 35 km và cách thành phố Quy Nhơn khoảng 70km</div>
+                                    </div>
+                                    <div>
+                                        <div>Giới thiệu:</div>
+                                        <div>Ghềnh Đá Đĩa được mệnh danh là tuyệt tác thiên nhiên của biển Việt Nam nói chung và tỉnh Phú Yên nói riêng. Nên nếu bạn đang ở Quy Nhơn nhưng vẫn mong muốn được chiêm ngưỡng tuyệt tác thiên nhiên này thì hoàn toàn có thể nhé!</div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>Liên hệ tour: 0123456789</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="address">
+                        <div>
+                            <div>
+                                <div>
+                                    <h2>Địa điểm và thông tin liên hệ</h2>
+                                    <span>
+                                        <span>
+                                            <img alt="" src="https://maps.google.com/maps/api/staticmap?&amp;channel=ta.desktop.restaurant_review&amp;zoom=15&amp;size=347x137&amp;scale=1&amp;client=gme-tripadvisorinc&amp;format=jpg&amp;sensor=false&amp;language=vi_VN&amp;center=21.033688,105.854881&amp;maptype=roadmap&amp;&amp;markers=icon:http%3A%2F%2Fc1.tacdn.com%2F%2Fimg2%2Fmaps%2Ficons%2Fcomponent_map_pins_v1%2FR_Pin_Small.png|21.033688,105.854881&amp;signature=Q0tnXrfTAL6Y-TfJfkih_iF7iYE="></span></span><div class="cSPba bKBJS Me"><span class="ui_icon map-pin-fill bPFFU">
+                                        </span>
+                                        <span>
+                                            <a>
+                                                <span>Xã An Ninh Đông, huyện Tuy An, tỉnh Phú Yên</span>
+                                                <span></span>
+                                            </a>
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <span></span>
+                                        <span>
+                                            <div>
+                                                <b>70km</b>
+                                                từ thành phố Quy Nhơn
+                                            </div>
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <span>
+                                                <a href="mailto:hiddengemcoffee@gmail.com?subject=?">
+                                                    <span></span>
+                                                    <span>E-mail:</span>
+                                                    <span></span>
+                                                </a></span>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div>
+                                                <a href="tel:+84 123 45 67 89"><span>
+                                                    <span></span>
+                                                    <span>+84 123456789</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        
+        
+        <!------------------------------------>
+    <div class="foot"><?php require_once 'footer.php';?></div>
+
+</body>
+</html>
