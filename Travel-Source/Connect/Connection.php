8c9e831d58bb56e79e75f8a8087ef63c58f20ed5
caominhbinh282 <80211677+caominhbinh282@users.noreply.github.com>
2021-12-11 14:10:17 +0700

    update connection set "utf8" for sql

--- Travel-Source/Connect/Connection.php
+++ Travel-Source/Connect/Connection.php
@@ -9,9 +9,12 @@ $conn = mysqli_connect($servername, $username, $password,"travels");
 
 
 // Check connection
-if (!$conn) {
+if($conn){
+  mysqli_query($conn,"SET NAMES 'utf8'");
+}else{
   die("Connection failed: " . mysqli_connect_error());
 }
+
 
 	
  ?>
